d1d3df67f1c96e3d4c68548f66f34184d5d77531
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2019-04-03 13:57:22 -0400

    rename to bigsql

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='bsql',
+    name='bigsql',
     version='1.0.0',
     author='big_J',
     url='https://gitlab.com/b1g_J/bsql',
     description='Just another ORM',
-    packages=['bsql'],
+    packages=['bigsql'],
     install_requires=['flask-mysql==1.4.0', 'pymysql==0.9.3', 'scanf==1.5.2'],
 )
