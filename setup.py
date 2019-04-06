3f2bae21bb09869578ef735dd99828c7c9d10e1a
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2019-04-05 23:23:28 -0400

    change url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='bigsql',
     version='1.0.0',
     author='big_J',
-    url='https://gitlab.com/b1g_J/bsql',
+    url='https://gitlab.com/b1g_J/bigsql',
     description='Just another ORM',
     packages=['bigsql'],
     install_requires=['flask-mysql==1.4.0', 'pymysql==0.9.3', 'scanf==1.5.2'],
